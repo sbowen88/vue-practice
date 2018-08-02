be90cd97b2868040d2e3e2c2fa7188852fe685e7
sbowen88 <sbowey@gmail.com>
2018-08-02 14:43:16 -0600

    second commit

---
+++ traversy/src/components/test.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="test">
+        <h1>{{title}}</h1>
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {
+      title: "Hello World"
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
+
